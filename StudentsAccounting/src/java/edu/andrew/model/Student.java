95e1819b8b1508348b1e110716bb20d30e70fd84
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-23 02:54:18 +0300

    add model classes

--- StudentsAccounting/src/java/edu/andrew/model/Student.java
+++ StudentsAccounting/src/java/edu/andrew/model/Student.java
@@ -5,5 +5,104 @@ package edu.andrew.model;
  * @author Andrew
  */
 public class Student {
+    private int id, user_id, course;
+    private String lastName, firstName, middleName, phoneNumber, birthDate, group, faculty;
+
+    public Student(int id, int user_id, String lastName, String firstName, String middleName, String birthDate, String phoneNumber, String faculty, int course, String group) {
+        this.id = id;
+        this.user_id = user_id;
+        this.course = course;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.group = group;
+        this.faculty = faculty;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
     
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", user_id=" + user_id + ", course=" + course + ", lastName=" + lastName + ", firstName=" + firstName + ", middleName=" + middleName + ", phoneNumber=" + phoneNumber + ", birthDate=" + birthDate + ", group=" + group + ", faculty=" + faculty + '}';
+    }
 }
--- StudentsAccounting/src/java/edu/andrew/model/User.java
+++ StudentsAccounting/src/java/edu/andrew/model/User.java
@@ -5,5 +5,86 @@ package edu.andrew.model;
  * @author Andrew
  */
 public class User {
+    private int id;
+    private String login, password, lasName, firstName, middleName, email, status;
     
+    public User(int id, String login, String password, String lasName, String firstName, String middleName, String email, String status) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.lasName = lasName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.email = email;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", login=" + login + ", password=" + password + ", lasName=" + lasName + ", firstName=" + firstName + ", middleName=" + middleName + ", email=" + email + ", status=" + status + '}';
+    }
 }
