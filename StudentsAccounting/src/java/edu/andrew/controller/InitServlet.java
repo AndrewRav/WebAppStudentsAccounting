7210af80e64f608431d9ad3ca45b42e88168f152
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-24 04:32:55 +0300

    create InitServlet and rename UsersDao to UserDao

--- StudentsAccounting/src/java/edu/andrew/controller/InitServlet.java
+++ StudentsAccounting/src/java/edu/andrew/controller/InitServlet.java
@@ -1,11 +1,34 @@
 package edu.andrew.controller;
 
+import edu.andrew.dao.Dao;
+import edu.andrew.dao.impl.StudentDao;
+import edu.andrew.dao.impl.UserDao;
+import edu.andrew.service.Service;
+import edu.andrew.service.impl.StudentServiceImpl;
+import edu.andrew.service.impl.UserServiceImpl;
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
 
 /**
  *
  * @author Andrew
  */
 public abstract class InitServlet extends HttpServlet {
+    private @Resource(name = "jdbc/studentdb") // Использование листнера
+    DataSource dataSource;
+    protected Dao studentDao;
+    protected Dao userDao;
+    protected Service studentService;
+    protected Service userService;
+
+    @Override
+    public void init() { // Инициализация объектов с которыми работает сервлеты
+        studentDao = new StudentDao(dataSource);
+        studentService = new StudentServiceImpl(studentDao);
+        
+        userDao = new UserDao(dataSource);
+        userService = new UserServiceImpl(userDao);
+    }  
     
 }
