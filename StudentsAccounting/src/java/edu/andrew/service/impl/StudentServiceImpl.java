79a4ec4b4be8419e7d794ee4f84c2721caa0770a
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-24 04:03:53 +0300

    add initial service implementations

--- StudentsAccounting/src/java/edu/andrew/service/impl/StudentServiceImpl.java
+++ StudentsAccounting/src/java/edu/andrew/service/impl/StudentServiceImpl.java
@@ -1,11 +1,38 @@
 package edu.andrew.service.impl;
 
-import edu.andrew.service.StudentService;
+import edu.andrew.model.Student;
+import edu.andrew.service.Service;
+import java.util.Set;
 
 /**
  *
  * @author Andrew
  */
-public class StudentServiceImpl implements StudentService {
+public class StudentServiceImpl implements Service<Student> {
+
+    @Override
+    public boolean create(Student student) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Set read() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean update(int id, String newName) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Student getById(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
--- StudentsAccounting/src/java/edu/andrew/service/impl/UserServiceImpl.java
+++ StudentsAccounting/src/java/edu/andrew/service/impl/UserServiceImpl.java
@@ -1,11 +1,38 @@
 package edu.andrew.service.impl;
 
-import edu.andrew.service.UserService;
+import edu.andrew.model.User;
+import edu.andrew.service.Service;
+import java.util.Set;
 
 /**
  *
  * @author Andrew
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements Service<User> {
+
+    @Override
+    public boolean create(User user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Set read() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean update(int id, String newName) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public User getById(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
