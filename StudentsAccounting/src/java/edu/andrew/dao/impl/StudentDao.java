4ce4deb0358ef8861806114be16da69bcd170ec1
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-09-25 03:18:41 +0300

    update methods to work with database

--- StudentsAccounting/src/java/edu/andrew/dao/impl/StudentDao.java
+++ StudentsAccounting/src/java/edu/andrew/dao/impl/StudentDao.java
@@ -2,6 +2,12 @@ package edu.andrew.dao.impl;
 
 import edu.andrew.dao.Dao;
 import edu.andrew.model.Student;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
 import java.util.Set;
 import javax.sql.DataSource;
 
@@ -18,22 +24,81 @@ public class StudentDao implements Dao<Student> {
     
     @Override
     public int create(Student student) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        try (Connection conn = dataSource.getConnection()) {//установление соединения (получение соединения)
+            int id;
+            
+            String sql = "INSERT INTO student (name) VALUES (?)";//формирование запроса добавления
+            try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {//в наш подготовленный запрос передаём строку с запросом
+                preparedStatement.setString(1, student.getFirstName());//переданный запрос меняем, указываем позицию ? и чем мы его занимаем, отсчёт с 1
+                int affectedRows = preparedStatement.executeUpdate();//количество добавленных строчек
+
+                if (affectedRows == 0) {// если строчки не добавлены выбрасывается исключение 
+                    throw new SQLException("Creating user failed, no rows affected.");
+                }
+                sql = "SELECT * FROM student ORDER BY id DESC LIMIT 1;";//сортируем в обратном порядке записи(от максимального до минимального) в бд и получаем первую строчку
+                try (Statement st = conn.createStatement(); ResultSet rs = st.executeQuery(sql)) {
+                    if (rs.next()) {//если результат имеет хотя бы 1у запись
+                        id = rs.getInt("id"); //берём id свежесозданной строчки 
+                    } else {
+                        throw new SQLException("Creating user failed, no ID obtained.");
+                    }
+                }
+            }
+            return id;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return -1;
+        } 
     }
 
     @Override
     public Set<Student> read() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        String req = "Select * from student";
+        Set<Student> groups;
+        try (Connection conn = dataSource.getConnection()) {            
+            try (Statement st = conn.createStatement(); ResultSet rs = st.executeQuery(req)) {
+                groups = new HashSet<>();
+                while (rs.next()) {//пока есть записи 
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    groups.add(new Student(id, name));
+                }
+            }
+            return groups;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        } 
     }
 
     @Override
     public int update(Student student) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "update student set name=? where id=? ";
+            try (PreparedStatement ps = conn.prepareStatement(query)) {
+                ps.setString(1, student.getFirstName());
+                ps.setInt(2, student.getId());
+                return ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return -1;
+        }
     }
 
     @Override
     public int delete(Student student) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try (Connection conn = dataSource.getConnection()) {            
+            String query = "delete from student where id = ?";
+            try (PreparedStatement preparedStmt = conn.prepareStatement(query)) {
+                preparedStmt.setInt(1, student.getId());
+                int res = preparedStmt.executeUpdate();
+                return res;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return -1;
+        }
     }
     
 }
