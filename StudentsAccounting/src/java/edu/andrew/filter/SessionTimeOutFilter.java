73eda730865408b2995dc54355a7d6fa0b22a469
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2023-10-18 02:16:56 +0300

    add session timeout

---
+++ StudentsAccounting/src/java/edu/andrew/filter/SessionTimeOutFilter.java
@@ -0,0 +1,45 @@
+package edu.andrew.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Andrew
+ */
+@WebFilter(urlPatterns = {"/updateUser", "/updateStudent"})
+public class SessionTimeOutFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpSession session = request.getSession(false);
+        if (session != null && !session.isNew()) {
+            chain.doFilter(req, resp);
+        } else {
+            if (session != null) {
+                session.invalidate();
+            }
+
+            req.getRequestDispatcher("/WEB-INF/jsp/timeout.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
